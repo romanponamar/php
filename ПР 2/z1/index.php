cf9286ae1c8fd5cb3efe3aa7845f3cac7ab95228
romanponamar <nuclearfish38@gmail.com>
2026-04-27 17:49:35 +0600

    +z1 +z2

---
+++ ПР 2/z1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+<?php
+	echo "<h1>Основы программирования</h1>";
+	echo "<h2>Введение в PHP<h/2>";
+	echo "<hr>";
+	echo "<h2>Начинаем программировать в среде PHP</h2>";
+?>
+</body>
+</html>	
